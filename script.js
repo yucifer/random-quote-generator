71a2896ae174ed444341db4844b93a3ea8bdf3f7
yucifer <yashjamesbond71@gmail.com>
2021-01-18 14:10:47 +0530

    created a random quote generator

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
+// Show Loading
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Get Quote from API
+async function getQuote() {
+  showLoadingSpinner();
+  //   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  apiUrl = "https://api.quotable.io/random";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const { content, author } = data;
+    if (content.length > 50) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = content;
+    authorText.innerText = author;
+    removeLoadingSpinner();
+  } catch (error) {
+    console.log("whoops! no quote", error);
+    getQuote();
+  }
+}
+
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+twitterBtn.addEventListener("click", tweetQuote);
+newQuoteBtn.addEventListener("click", getQuote);
+
+// On Load
+getQuote();
